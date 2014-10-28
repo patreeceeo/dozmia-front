f47300b1604b6803a5f29b21f88432a21811f90c
Patrick Canfield <patrickcandoit@gmail.com>
2014-10-28 11:12:33 -0700

    fix album art wall

--- src/dozmia-home-view.js
+++ src/dozmia-home-view.js
@@ -2,6 +2,12 @@
   "use strict";
 
   dozmia.HomeView = dozmia.BaseView.extend({
+    initialize: function () {
+      this.totalWidth = window.screen.width || 1920;
+      this.totalHeight = window.screen.height || 1080;
+      this.nTiles = 40;
+      this.tileWidth = Math.sqrt((this.totalWidth * this.totalHeight) / this.nTiles) / 1.5;
+    },
     html: function () {
       return "<div class=\"dozmia-album-art-wall\">"+this._generateRandomArtMarkup()+"</div>";
     },
@@ -9,23 +15,31 @@
       var msnry, $artWall;
       $artWall = this.$(".dozmia-album-art-wall");
 
+      $artWall
+        .width(this.totalWidth)
+        .height(this.totalHeight);
+      $(".dozmia-album-art:not(.dozmia-album-art--half)")
+        .width(this.tileWidth * 2)
+        .height(this.tileWidth * 2);
+      $(".dozmia-album-art.dozmia-album-art--half")
+        .width(this.tileWidth)
+        .height(this.tileWidth);
       msnry = new Masonry($artWall[0], {
-        columnWidth: 90,
+        columnWidth: this.tileWidth,
         itemSelector: ".dozmia-album-art"
       });
     },
     _generateRandomArtMarkup: function () {
-      var totalWidth, totalHeight, nTiles;
-      totalWidth = window.screen.width || 1920;
-      totalHeight = window.screen.height || 1080;
-      nTiles = (totalWidth * totalHeight) / (90 * 90);
-      return dozmia.u.times(nTiles, function () {
+      var layout = dozmia.u.random(1);
+      return dozmia.u.times(this.nTiles, function (index) {
         var size;
-        size = dozmia.u.random(1);
+        size = (index + layout) % 2;
         if(size === 0) {
           return "<div class=\"dozmia-album-art\"></div>";
         } else {
-          return "<div class=\"dozmia-album-art dozmia-album-art--half\"></div>";
+          return "<div class=\"dozmia-album-art dozmia-album-art--half\"></div>"+
+                 "<div class=\"dozmia-album-art dozmia-album-art--half\"></div>"+
+                 "<div class=\"dozmia-album-art dozmia-album-art--half\"></div>";
         }
       }).join("");
     }
